00857f585ead8ae9c22fe364cff4caadad92ff6f
NaziaBeaghum <nazia.mk2@gmail.com>
2023-11-06 12:59:53 +0530

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,22 +2,29 @@ import React from 'react'
 import { Stack } from '@mui/material'
 import { Link } from 'react-router-dom'
 import SearchInput from './SearchInput'
+import { useNavigate } from 'react-router-dom'
 
-const Header = () => 
-   (
+const Header = () =>{
+   const navigate=useNavigate()
+
+  const handleClick=()=>{
+    navigate('/')
+  }
+ return(
+   
     <Stack direction="row" 
     alignItems="center"
     p={2}
     sx={{top:0,justifyContent:'space-between'}}>
 
-     <Link to="/">
-        <img src="assets/logo.png" alt="VideoApp" height={65}/>
-     </Link> 
+   
+        <img src="assets/logo.png" alt="VideoApp" height={65} onClick={handleClick}/>
+      
 
         
         <SearchInput/>
     </Stack>
   )
 
-
+   }
 export default Header
